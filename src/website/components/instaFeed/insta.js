59e12099cce4782fc904a06ee0e35c58fed5f862
Benjamin Konopka <benjamin.konopka@virtual-identity.com>
2019-07-28 18:05:34 +0200

    adding instafeed, style overlay and grid layout

--- src/website/components/instaFeed/insta.js
+++ src/website/components/instaFeed/insta.js
@@ -1,17 +1,31 @@
-
-// fetch data
 (async () => {
-  const res = await fetch(`https://api.instagram.com/v1/users/self/media/recent?access_token=14590747835.1677ed0.05084289959a41e99c42caf952ed224c`)
-  const json = await res.json();
+  const json = await fetch(`https://api.instagram.com/v1/users/self/media/recent?access_token=14590747835.1677ed0.05084289959a41e99c42caf952ed224c`).then(res => res.json());
   const { data } = json;
-  console.log(data[0].images.low_resolution.url)
+  console.log(data)
 
-  // get class instafeed
-  const renderInstaFeed = `<img src='${data[0].images.low_resolution.url}'/>`
+  const createHashtagHtml = tag => `<li>#${tag}</li>`;
+
+  const createHashtagsHtml = post => `
+    <ul class="insta__tags">
+      ${post.tags.slice(0, 5).map(createHashtagHtml).join('')}
+    </ul>
+  `;
+
+  const createLikesHtml = post => `
+    <div class="insta__likes">
+      ❤️${post.likes.count}
+    </div>
+  `;
+
+  const createInstaPostHtml = instaPost => `
+  <a href="${instaPost.link}" target="blank" class="insta">
+    <img src='${instaPost.images.low_resolution.url}'/>
+    <div class="insta__overlay">
+      ${createLikesHtml(instaPost)}
+      ${instaPost.tags.length > 0 ? createHashtagsHtml(instaPost) : ''}
+    </div>
+  </a>
+  `;
   const instafeed = document.getElementsByClassName('instafeed')[0];
-  instafeed.innerHTML = renderInstaFeed;
+  instafeed.innerHTML = [...data, ...data].map(createInstaPostHtml).join('');
 })()
-
-// loop over response
-
-// render template to site

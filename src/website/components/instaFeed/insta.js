92c805b5e90cc1205353545e1824285acd498348
Benjamin Konopka <benjamin.konopka@virtual-identity.com>
2019-07-29 17:15:54 +0200

    adapt instagram feed

--- src/website/components/instaFeed/insta.js
+++ src/website/components/instaFeed/insta.js
@@ -1,7 +1,7 @@
 (async () => {
   const json = await fetch(`https://api.instagram.com/v1/users/self/media/recent?access_token=14590747835.1677ed0.05084289959a41e99c42caf952ed224c`).then(res => res.json());
   const { data } = json;
-  console.log(data)
+  console.log(json);
 
   const createHashtagHtml = tag => `<li>#${tag}</li>`;
 
@@ -13,19 +13,30 @@
 
   const createLikesHtml = post => `
     <div class="insta__likes">
-      ❤️${post.likes.count}
+      ${post.likes.count} likes <i class="fa fa-heart"></i>
     </div>
   `;
 
-  const createInstaPostHtml = instaPost => `
-  <a href="${instaPost.link}" target="blank" class="insta">
-    <img src='${instaPost.images.low_resolution.url}'/>
-    <div class="insta__overlay">
-      ${createLikesHtml(instaPost)}
-      ${instaPost.tags.length > 0 ? createHashtagsHtml(instaPost) : ''}
+  const createCommentsHtml = comment => `
+    <div class="insta__commentsCount">
+      ${comment.comments.count} Kommentare <i class="far fa-comment"></i>
     </div>
-  </a>
+  `
+
+  const createInstaPostHtml = instaPost => `
+  <div class="insta__feed">
+    <a href="${instaPost.link}" target="blank" class="insta__img">
+      <img src='${instaPost.images.low_resolution.url}'/>
+      <div class="insta__overlay">
+      <i class="fa fa-heart fa-3x"></i>
+      </div>
+    </a>
+    <div class="insta__infos">
+      ${createLikesHtml(instaPost)}
+      ${createCommentsHtml(instaPost)}
+    </div>
+  </div>
   `;
-  const instafeed = document.getElementsByClassName('instafeed')[0];
-  instafeed.innerHTML = [...data, ...data].map(createInstaPostHtml).join('');
+  const instafeed = document.getElementsByClassName('insta__feeds')[0];
+  instafeed.innerHTML = data.map(createInstaPostHtml).join('');
 })()
